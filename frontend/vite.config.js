7c34b15df9c4968f9c64a09fe80bd8cedffce957
AzzikraPraqastaKusuma123 <azzikrapraqasta1@gmail.com>
2025-10-14 20:54:04 +0700

    update perbaikikan error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url'; 
 
-// https://vite.dev/config/
+// Dapatkan __dirname secara manual untuk ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Arahkan semua impor 'react' ke satu lokasi (instance) yang sama
+      'react': path.resolve(__dirname, 'node_modules/react'),
+    },
+  },
+});
